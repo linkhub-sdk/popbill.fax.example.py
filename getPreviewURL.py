8c17843f029bde5f2839c2d9971490b347eb8532
kimhyunjin <khjzzm@gmail.com>
2018-11-29 09:43:53 +0900

    update date 2018-11-29

--- getPreviewURL.py
+++ getPreviewURL.py
@@ -14,12 +14,12 @@ faxService = FaxService(testValue.LinkID, testValue.SecretKey)
 faxService.IsTest = testValue.IsTest
 
 '''
-팩스 전송내역 목록 팝업 URL을 반환합니다.
+접수한 팩스 전송건에 대한 미리보기 팝업 URL을 반환합니다.
 - 보안정책으로 인해 반환된 URL은 30초의 유효시간을 갖습니다.
 '''
 
 try:
-    print("=" * 15 + " 팩스 전송내역 목록 팝업 URL 확인 " + "=" * 15)
+    print("=" * 15 + " 팩스 미리보기 팝업 URL 확인 " + "=" * 15)
 
     # 팝빌회원 사업자번호
     CorpNum = testValue.testCorpNum
--- testValue.py
+++ testValue.py
@@ -3,7 +3,7 @@
 '''
  팝빌 팩스 API Python SDK Example
  - Python SDK 연동환경 설정방법 안내 : http://blog.linkhub.co.kr/581
- - 업데이트 일자 : 2017-08-29
+ - 업데이트 일자 : 2018-11-29
  - 연동 기술지원 연락처 : 1600-9854 / 070-4304-2991~2
  - 연동 기술지원 이메일 : code@linkhub.co.kr
  <테스트 연동개발 준비사항>
