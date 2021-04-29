af979bffbbf2d2a4225bacaae3e2f8d6ff72d3b1
1jae <wjkim@linkhub.co.kr>
2021-04-29 19:27:09 +0900

    added getPaymentURL, getUseHistoryURL API

---
+++ getUseHistoryURL.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# code for console Encoding difference. Dont' mind on it
+import sys
+import imp
+imp.reload(sys)
+try: sys.setdefaultencoding('UTF8')
+except Exception as E: pass
+
+import testValue
+
+from popbill import FaxService, PopbillException
+
+faxService = FaxService(testValue.LinkID, testValue.SecretKey)
+faxService.IsTest = testValue.IsTest
+faxService.IPRestrictOnOff = testValue.IPRestrictOnOff
+faxService.UseStaticIP = testValue.UseStaticIP
+faxService.UseLocalTimeYN = testValue.UseLocalTimeYN
+
+'''
+팝빌 연동회원 포인트 사용내역 URL을 반환합니다.
+- URL 보안정책에 따라 반환된 URL은 30초의 유효시간을 갖습니다.
+- https://docs.popbill.com/fax/python/api#GetUseHistoryURL
+'''
+
+try:
+    print("=" * 15 + " 연동회원 포인트 사용내역 URL 확인 " + "=" * 15)
+
+    # 팝빌회원 사업자번호
+    CorpNum = testValue.testCorpNum
+
+    # 팝빌회원 아이디
+    UserID = testValue.testUserID
+
+    url = faxService.getUseHistoryURL(CorpNum, UserID)
+
+    print("URL: %s" % url)
+
+except PopbillException as PE:
+    print("Exception Occur : [%d] %s" % (PE.code , PE.message))
