ce2f0ee5a474abd97c34d36c39734e71f481e553
ProWON <pro_won45@naver.com>
2021-10-05 10:04:06 +0900

    fixed comment UswStaticIP

--- testValue.py
+++ testValue.py
@@ -32,7 +32,7 @@ testUserID = "testkorea"
 # 발급토큰 IP 제한기능 활성화 여부 (권장-True)
 IPRestrictOnOff = True
 
-# 팝빌 API 서비스 고정 IP 사용여부(GA), true-사용, false-미사용, 기본값(false)
+# 팝빌 API 서비스 고정 IP 사용여부, true-사용, false-미사용, 기본값(false)
 UseStaticIP = False
 
 #로컬시스템 시간 사용여부, 권장(True)
