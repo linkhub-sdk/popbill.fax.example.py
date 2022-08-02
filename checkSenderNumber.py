cf4bb8f553692f229835d2786c7abd454a0c31ef
ZIZON97 <wlgjs101@gmail.com>
2022-08-02 10:13:39 +0900

    added CheckSenderNumber API.

---
+++ checkSenderNumber.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# code for console Encoding difference. Dont' mind on it
+import sys
+import imp
+
+imp.reload(sys)
+try:
+    sys.setdefaultencoding('UTF8')
+except Exception as E:
+    pass
+
+import testValue
+
+from popbill import FaxService, PopbillException
+
+faxService = FaxService(testValue.LinkID, testValue.SecretKey)
+faxService.IsTest = testValue.IsTest
+faxService.IPRestrictOnOff = testValue.IPRestrictOnOff
+faxService.UseStaticIP = testValue.UseStaticIP
+faxService.UseLocalTimeYN = testValue.UseLocalTimeYN
+
+'''
+팩스 발신번호 등록여부를 확인합니다.
+- 발신번호 상태가 '승인'인 경우에만 리턴값 'Response'의 변수 'code'가 1로 반환됩니다.
+- https://docs.popbill.com/fax/python/api#CheckSenderNumber
+'''
+
+try:
+    print("=" * 15 + " 발신번호 등록여부 확인 " + "=" * 15)
+
+    # 팝빌회원 사업자번호
+    CorpNum = testValue.testCorpNum
+
+    # 확인할 발신번호
+    senderNumber = ""
+
+    result = faxService.checkSenderNumber(CorpNum, senderNumber)
+
+    print("처리결과 : [%d] %s" % (result.code, result.message))
+
+except PopbillException as PE:
+    print("Exception Occur : [%d] %s" % (PE.code, PE.message))
